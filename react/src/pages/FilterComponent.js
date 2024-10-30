c580bffa4b30b023480e868da123f300d59b23df
Jeremy Michoma <jm7048@princeton.edu>
2024-10-30 17:38:00 -0400

    fixing routes for filter to hardcode results page

--- react/src/pages/FilterComponent.js
+++ react/src/pages/FilterComponent.js
@@ -1,53 +1,49 @@
 // src/components/FilterComponent.js
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../FilterComponent.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../FilterComponent.css";
 
 const FilterComponent = () => {
-  const [residentialCollege, setResidentialCollege] = useState('');
-  const [hall, setHall] = useState('');
-  const [floor, setFloor] = useState('');
-  const [occupancy, setOccupancy] = useState('');
+  const [residentialCollege, setResidentialCollege] = useState("");
+  const [hall, setHall] = useState("");
+  const [floor, setFloor] = useState("");
+  const [occupancy, setOccupancy] = useState("");
   const [error, setError] = useState(false);
 
   const navigate = useNavigate();
 
   // Hardcoded list of residential colleges
   const residentialColleges = [
-    'Butler College',
-    'Forbes College',
-    'Mathey College',
-    'New College West',
-    'Rockefeller College',
-    'Whitman College',
-    'Yeh College'
+    "Butler College",
+    "Forbes College",
+    "Mathey College",
+    "New College West",
+    "Rockefeller College",
+    "Whitman College",
+    "Yeh College",
   ];
 
   // Hardcoded halls for Whitman College
   const whitmanHalls = [
-    '1981 Hall',
-    'Fisher Hall',
-    'Lauritzen Hall',
-    'Murley-Pivirotto Family Tower',
-    'Wendell B Hall',
-    'Wendell C Hall'
+    "1981 Hall",
+    "Fisher Hall",
+    "Lauritzen Hall",
+    "Murley-Pivirotto Family Tower",
+    "Wendell B Hall",
+    "Wendell C Hall",
   ];
 
   // Hardcoded floors for Wendell B Hall
   const wendellBFloors = [
-    'Ground Floor',
-    '1st Floor',
-    '2nd Floor',
-    '3rd Floor',
-    '4th Floor'
+    "Ground Floor",
+    "1st Floor",
+    "2nd Floor",
+    "3rd Floor",
+    "4th Floor",
   ];
 
   // Hardcoded occupancies for Wendell B Hall
-  const wendellBOccupancies = [
-    'Single',
-    'Double',
-    'Quad'
-  ];
+  const wendellBOccupancies = ["Single", "Double", "Quad"];
 
   const handleSubmit = () => {
     // Check if the "Residential College" field is filled out
@@ -59,26 +55,30 @@ const FilterComponent = () => {
     setError(false); // Reset error if residentialCollege is selected
 
     // Transform hall and floor names for the URL
-    const formattedHall = hall.replace(' Hall', '').toLowerCase().replace(/\s+/g, '-'); // Remove "Hall" and replace spaces with hyphens
-    const formattedFloor = floor.toLowerCase().replace(/\s+/g, '-'); // Replace spaces with hyphens
+    // const formattedHall = hall
+    //   .replace(" Hall", "")
+    //   .toLowerCase()
+    //   .replace(/\s+/g, "-"); // Remove "Hall" and replace spaces with hyphens
+    // const formattedFloor = floor.toLowerCase().replace(/\s+/g, "-"); // Replace spaces with hyphens
 
     // Navigate to the new URL
-    navigate(`/floorplans/${formattedHall}-${formattedFloor}`);
+    // navigate(`/floorplans/${formattedHall}-${formattedFloor}`);
+    navigate(`/floorplans`);
   };
 
   // Reset function to clear all selections
   const handleResetFilters = () => {
-    setResidentialCollege('');
-    setHall('');
-    setFloor('');
-    setOccupancy('');
+    setResidentialCollege("");
+    setHall("");
+    setFloor("");
+    setOccupancy("");
     setError(false);
   };
 
   return (
     <div className="filter-container">
-      <h1 className='filter-container-title'>Welcome to TigerRooms</h1>
-      <h3 className='filter-container-title'>Looking for an available room?</h3>
+      <h1 className="filter-container-title">Welcome to TigerRooms</h1>
+      <h3 className="filter-container-title">Looking for an available room?</h3>
       <div className="dropdown-container">
         <div className="dropdown-group">
           <label htmlFor="residentialCollege">Residential College*</label>
@@ -86,50 +86,90 @@ const FilterComponent = () => {
             id="residentialCollege"
             value={residentialCollege}
             onChange={(e) => setResidentialCollege(e.target.value)}
-            style={{ borderColor: error ? 'red' : '' }} // Highlight border if error
+            style={{ borderColor: error ? "red" : "" }} // Highlight border if error
           >
-            <option value="" className="placeholder-option">Select Residential College</option>
+            <option value="" className="placeholder-option">
+              Select Residential College
+            </option>
             {residentialColleges.map((college, idx) => (
-              <option key={idx} value={college}>{college}</option>
+              <option key={idx} value={college}>
+                {college}
+              </option>
             ))}
           </select>
-          {error && <p style={{ color: 'red', fontSize: '0.9em' }}>Please select a Residential College.</p>}
+          {error && (
+            <p style={{ color: "red", fontSize: "0.9em" }}>
+              Please select a Residential College.
+            </p>
+          )}
         </div>
 
         <div className="dropdown-group">
           <label htmlFor="hall">Hall</label>
-          <select id="hall" value={hall} onChange={(e) => setHall(e.target.value)}>
-            <option value="" className="placeholder-option">Select Hall</option>
+          <select
+            id="hall"
+            value={hall}
+            onChange={(e) => setHall(e.target.value)}
+          >
+            <option value="" className="placeholder-option">
+              Select Hall
+            </option>
             {whitmanHalls.map((hallOption, idx) => (
-              <option key={idx} value={hallOption}>{hallOption}</option>
+              <option key={idx} value={hallOption}>
+                {hallOption}
+              </option>
             ))}
           </select>
         </div>
 
         <div className="dropdown-group">
           <label htmlFor="floor">Floor</label>
-          <select id="floor" value={floor} onChange={(e) => setFloor(e.target.value)}>
-            <option value="" className="placeholder-option">Select Floor</option>
+          <select
+            id="floor"
+            value={floor}
+            onChange={(e) => setFloor(e.target.value)}
+          >
+            <option value="" className="placeholder-option">
+              Select Floor
+            </option>
             {wendellBFloors.map((sz, idx) => (
-              <option key={idx} value={sz}>{sz}</option>
+              <option key={idx} value={sz}>
+                {sz}
+              </option>
             ))}
           </select>
         </div>
 
         <div className="dropdown-group">
           <label htmlFor="occupancy">Occupancy</label>
-          <select id="occupancy" value={occupancy} onChange={(e) => setOccupancy(e.target.value)}>
-            <option value="" className="placeholder-option">Select Occupancy</option>
+          <select
+            id="occupancy"
+            value={occupancy}
+            onChange={(e) => setOccupancy(e.target.value)}
+          >
+            <option value="" className="placeholder-option">
+              Select Occupancy
+            </option>
             {wendellBOccupancies.map((occ, idx) => (
-              <option key={idx} value={occ}>{occ}</option>
+              <option key={idx} value={occ}>
+                {occ}
+              </option>
             ))}
           </select>
         </div>
       </div>
 
       <div className="button-container">
-        <button className='submit-button' onClick={handleSubmit}><strong>SUBMIT</strong></button>
-        <button className='reset-button' onClick={handleResetFilters} style={{ marginLeft: '10px' }}><strong>RESET</strong></button>
+        <button className="submit-button" onClick={handleSubmit}>
+          <strong>SUBMIT</strong>
+        </button>
+        <button
+          className="reset-button"
+          onClick={handleResetFilters}
+          style={{ marginLeft: "10px" }}
+        >
+          <strong>RESET</strong>
+        </button>
       </div>
     </div>
   );
