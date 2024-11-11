58a20124ff836e6d460175220c6b1a33b517219e
Anupta <anupta22@gmail.com>
2024-11-11 01:26:04 -0500

    Updated ReadMe to reflect deployment changes, commented PDFparser

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 # couldn't find creator to credit
 
-# PDF Parsing instructions
+# PDF Parsing instructions (outdated)
 
 1. Install tabula by executing
    pip install tabula-py
@@ -38,8 +38,42 @@ export APP_SECRET_KEY=example
 
 # Updated server build instructions
 
-1. Make sure the env variables are loaded, run `source .env` if they
-are not, or if the start command gives errors.
+1. Create the two .env files in the server/ and react/ directories
+respectively. Copy and paste them from the groupchat exactly.
 
-2. Execute `gunicorn --bind 127.0.0.1:$SERVER_PORT server:app` in the
-terminal to run the server.
+2. Make sure the .env variables are loaded, run `source .env` if they
+are not, or if the start command gives errors saying a variable is an
+empty string or NONE.
+
+3. Execute `gunicorn --bind 127.0.0.1:$SERVER_PORT server:app` in the
+terminal to run the server. This runs on localhost for now, but can be
+changed to 0.0.0.0 to test on all devices.
+
+# General Deployment Updates
+
+1. Anytime you import a new python module, add it to `requirements.txt`.
+When Render builds our server, it needs to install the newly added 
+packages.
+
+2. Anytime you import a new React module, add it to the list of packages
+installed above using `npm` (i.e. where sweetalert2 is currently). When
+Render builds our actual website, it needs these packages.
+
+2. All commits will by default automatically affect our deployment. If
+you want a commit to not be reflected in the deployment, add `[skip render]`
+or `[render skip]` to the beginning of your commit message.
+
+3. If your repository ever gets corrupted, just delete it, clone it back,
+and run `npm install` followed by the installation of any React modules
+installed above.
+
+4. The PDFparser has been updated to run without a JVM environment, but
+it lacks proper testing. Only using files of the format `Wendell*.pdf`
+for testing until the parser has been properly tested.
+
+5. Whenever you create a new JS page, use `const apiUrl = process.env.REACT_APP_API_URL;`
+to get the server URL at the top of the block, and if this is used in a useEffect,
+add it to the dependency array so that load errors do not occur. No more hardcoding!
+
+6. All server routes with the exception of the index MUST begin with `/api`. This
+makes debugging easier during deployment.
--- server/pdfparser.py
+++ server/pdfparser.py
@@ -76,7 +76,7 @@ def parse_pdf(filepath):
 
     concatenated_tables = pd.concat([first_page] + rem_pages, ignore_index=True)
     
-    # Drop NaN rows (rows with no data)
+    # Drop NaN rows
     processed_table = concatenated_tables.dropna(how='all')
 
     # Ensure the square footage column is numeric
@@ -113,101 +113,3 @@ def main():
 #-----------------------------------------------------------------------
 if __name__ == '__main__':
     main()
-
-#-----------------------------------------------------------------------
-
-# def parse_pdf(filepath):
-#     # Parse tables found in PDF into a list of dataframes
-#     tables_list = tabula.read_pdf(filepath, 
-#                                   multiple_tables=True, 
-#                                   pages='all',
-#                                   pandas_options={'header': None})
-    
-#     if not tables_list:
-#         raise ValueError("No tables found in the PDF.")
-    
-#     # Check for timestamp in format "Updated MM/DD/YYYY HH:MM AM/PM"
-#     last_updated = None
-#     updated_time_pattern = r'Updated\s*(\d{1,2}[/-]\d{1,2}[/-]\d{2,4})\s*(\d{1,2}:\d{2})\s*(AM|PM|am|pm)'
-    
-#     # Search for the timestamp across all tables
-#     for table in tables_list:
-#         for row in table.values.flatten():
-#             if isinstance(row, str):
-#                 match = re.search(updated_time_pattern, row)
-#                 # If the timestamp entry is found, save it
-#                 if match:
-#                     last_updated = f"{match.group(1)} {match.group(2)} {match.group(3)}"
-#                     break
-
-#         if last_updated:
-#             break
-    
-#     # If no last updated timestamp is found, raise error
-#     if not last_updated:
-#         raise ValueError("Last updated timestamp not found in the document.")
-    
-#     # Check for header columns of this form
-#     required_columns = ['college', 'building', 'room', 'type', 'sq. ft.']
-#     header_row_idx = None
-
-#     for table in tables_list:
-#         for row_idx, row in table.iterrows():
-#             row_lower = row.astype(str).str.lower().values
-#             if all(col in row_lower for col in required_columns):
-#                 header_row_idx = row_idx
-#                 break
-#         if header_row_idx is not None:
-#             break
-    
-#     # If no header row is found, throw an error
-#     if header_row_idx is None:
-#         raise ValueError("Header row not found.")
-
-#     # Drop all rows before the header row, resetting the index
-#     # It can be assumed that the header row will be in the first table
-#     first_page = tables_list[0]
-#     first_page_after_header = first_page.iloc[header_row_idx + 1:].reset_index(drop=True)
-
-#     # Concatenate this modified table with remaining tables
-#     rem_pages = tables_list[1:]
-#     concatenated_tables = pd.concat([first_page_after_header] + rem_pages, ignore_index=True)
-    
-#     # Drop NaN rows (rows with no data)
-#     processed_table = concatenated_tables.dropna(how='all')
-
-#     # Ensure the square footage column is numeric
-#     # It can be assumed that this is the last column
-#     processed_table.iloc[:, -1] = pd.to_numeric(processed_table.iloc[:, -1], errors='coerce').fillna(0).astype(int)
-
-#     return last_updated, processed_table
-
-# #-----------------------------------------------------------------------
-
-# def main():
-#     # Create argument parser
-#     descript = "PDF Parser application: convert PDF into pandas DF"
-#     parser = argparse.ArgumentParser(prog=f'{sys.argv[0]}',
-#                                      description=descript)
-#     parser.add_argument("filepath", type=str, metavar="filepath",
-#                         help="the string path to the PDF file")
-    
-#     # Get filepath from arguments
-#     args = parser.parse_args()
-#     filepath = args.filepath
-
-#     try:
-#         # Parse the PDF
-#         last_updated, tables = parse_pdf(filepath)
-        
-#         # Output the last updated time and the parsed data
-#         print(f"Timestamp: {last_updated}")
-#         print(tables.to_string(index=False))
-#     except Exception as e:
-#         # Handle exceptions gracefully
-#         print(f"Error: {e}")
-#         sys.exit(1)
-
-# #-----------------------------------------------------------------------
-# if __name__ == '__main__':
-#     main()
