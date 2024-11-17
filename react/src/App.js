56cea9dc9edeb1a9a19096c6e10e8180e4677605
sashakhoretonenko <sasha.khoretonenko@gmail.com>
2024-11-17 13:24:44 -0500

    more CAS

--- react/src/App.js
+++ react/src/App.js
@@ -14,83 +14,47 @@ const App = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
   const [username, setUsername] = React.useState(null);
 
-  // React.useEffect(() => {
-  //   if (username) {
-  //     console.log("Username is already present");
-  //     return;
-  //   }
+  React.useEffect(() => {
+    if (username) {
+      console.log("Username is already present");
+      return;
+    }
 
-  // Function to fetch user data
-  //   const fetchUserData = async () => {
-  //     try {
-  //       const response = await fetch(`${apiUrl}/api/user`, {
-  //         method: "GET",
-  //         credentials: "include",
-  //       });
+    // Function to fetch user data
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/api/user`, {
+          method: "GET",
+          credentials: "include",
+        });
 
-  //       if (response.status === 200) {
-  //         console.log("Fetched user data successfully");
-  //         const data = await response.json();
+        if (response.status === 200) {
+          console.log("Fetched user data successfully");
+          const data = await response.json();
 
-  //         if (data.status === "success" && data.username) {
-  //           console.log("User authenticated, setting username:", data.username);
-  //           setUsername(data.username); // Set username if authenticated
-  //         } else {
-  //           console.error("User not authenticated or username not available");
-  //           window.location.href = `${apiUrl}`; // Redirect to login page
-  //         }
-  //       } else if (response.status === 401) {
-  //         console.error("User not authenticated (401 status)");
-  //         window.location.href = `${apiUrl}`; // Redirect to login page
-  //       } else {
-  //         console.error("Unexpected response:", response);
-  //         window.location.href = `${apiUrl}`; // Redirect to login page
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching user data:", error);
-  //       window.location.href = `${apiUrl}`; // Redirect to login page
-  //       // window.location.href = `${apiUrl}`; // Redirect to login page
-  //     }
-  //   };
-
-  //   fetchUserData();
-  // }, []); // Only run effect when apiUrl or username changes
-
-  const fetchUserData = async () => {
-    try {
-      const response = await fetch(`${apiUrl}/api/user`, {
-        method: "GET",
-        credentials: "include",
-      });
-
-      if (response.status === 200) {
-        console.log("Fetched user data successfully");
-        const data = await response.json();
-
-        if (data.status === "success" && data.username) {
-          console.log("User authenticated, setting username:", data.username);
-          setUsername(data.username); // Set username if authenticated
+          if (data.status === "success" && data.username) {
+            console.log("User authenticated, setting username:", data.username);
+            setUsername(data.username); // Set username if authenticated
+          } else {
+            console.error("User not authenticated or username not available");
+            window.location.href = `${apiUrl}`; // Redirect to login page
+          }
+        } else if (response.status === 401) {
+          console.error("User not authenticated (401 status)");
+          window.location.href = `${apiUrl}`; // Redirect to login page
         } else {
-          console.error("User not authenticated or username not available");
+          console.error("Unexpected response:", response);
           window.location.href = `${apiUrl}`; // Redirect to login page
         }
-      } else if (response.status === 401) {
-        console.error("User not authenticated (401 status)");
+      } catch (error) {
+        console.error("Error fetching user data:", error);
         window.location.href = `${apiUrl}`; // Redirect to login page
-      } else {
-        console.error("Unexpected response:", response);
-        window.location.href = `${apiUrl}`; // Redirect to login page
+        // window.location.href = `${apiUrl}`; // Redirect to login page
       }
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-      window.location.href = `${apiUrl}`; // Redirect to login page
-    }
-  };
+    };
 
-  // Call fetchUserData on every render
-  React.useEffect(() => {
     fetchUserData();
-  });
+  }, [apiUrl, username]); // Only run effect when apiUrl or username changes
 
   return (
     <BrowserRouter>
